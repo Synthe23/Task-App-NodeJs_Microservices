4c4da63c03ae048c93da8722f74a6ce5b16dfd99
OmPrakash23 <mishraomm5000@gmail.com>
2026-04-09 10:05:06 +0530

    Implement RabbitMQ connection and task notification listener
    Establish RabbitMQ connection and set up message consumption for task notifications.

--- notification-service/index.js
+++ notification-service/index.js
@@ -1,1 +1,30 @@
+import amqp from "amqplib";
 
+let connection, channel;
+
+// Rabbit MQ connection
+async function start() {
+  try {
+    connection = await amqp.connect("amqp://localhost:5672");
+    channel = await connection.createChannel();
+
+    await channel.assertQueue("task_created");
+
+    console.log("Connected to RabbitMQ ✅");
+    console.log("Notification service is listening to messages! 🔔");
+
+    channel.consume("task_created", (message) => {
+      const taskData = JSON.parse(message.content.toString());
+
+      console.log("🔔 New Task Notification:", taskData.title);
+      console.log("📦 Full Task Data:", taskData);
+
+      channel.ack(message);
+    });
+
+  } catch (error) {
+    console.error("RabbitMQ connection error:", error.message);
+  }
+}
+
+start();
